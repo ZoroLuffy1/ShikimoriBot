2aff4e4c102eefc0d805653905e3363e4d90b3dc
ZoroLuffy <100777217+ZoroLuffy1@users.noreply.github.com>
2022-07-07 14:39:15 +0530

    Update shippering.py

--- Shikimori/modules/shippering.py
+++ Shikimori/modules/shippering.py
@@ -76,7 +76,7 @@ __New couple of the day may be chosen at 12AM {tomorrow}__"""
         await message.reply_text(e)
 
 
-__mod_name__ = "Couples / Shippering 💑"
+__mod_name__ = "Couples"
 __help__ = """
 *Couples / Shippering*
  ❍ `/couples` :  get couples of the day!
